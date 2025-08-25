1722bd41aaf0f1f2b25066b217c8eba760512864
Yu Song <109290434+jingmingliang@users.noreply.github.com>
2025-08-25 08:26:35 +0800

    Update ResNet-CoRe.py

--- Lib/models/ResNet-CoRe.py
+++ Lib/models/ResNet-CoRe.py
@@ -39,27 +39,6 @@ class DictConv2d(nn.Module):
 
         return out
 
-        # out, rc = self.dn(x)
-
-        # delta_closs = rc[1] - self.running_c_loss
-        # while delta_closs > 0.:
-        #     print("in the loop")
-        #     print(rc[1], self.running_c_loss)
-        #     self.dn.lmbd += 0.1
-        #     out, rc = self.dn(x)
-        #     delta_closs = rc[1] - self.running_c_loss
-        # return out
-
-    # def robust_forward(self, x):
-    #     out, rc = self.dn(x)
-    #
-    #     delta_closs = rc[1] - self.running_c_loss
-    #     while delta_closs > 0.:
-    #         print("in the loop")
-    #         self.dn.lmbd += 0.1
-    #         out, rc = self.dn(x)
-    #         delta_closs = rc[1] - self.running_c_loss
-    #     return out
 
 def conv3x3(in_planes, out_planes, stride=1):
     """3x3 convolution with padding"""
@@ -146,10 +125,15 @@ class ResNet(nn.Module):
 
     def __init__(self, block, layers, num_classes=100, zero_init_residual=False):
         super(ResNet, self).__init__()
-        self.inplanes = 3
-        self.conv1 = DictConv2d(in_channels=3, out_channels=self.inplanes, kernel_size=3, stride=1, padding=1,bias=False)
-        self.bn1 = nn.BatchNorm2d(3)
-        #self.relu = nn.ReLU(inplace=True)
+        self.inplanes = 64
+        self.conv1 = nn.Conv2d(3, 64, kernel_size=7, stride=2, padding=3,
+                               bias=False)
+        self.bn1 = nn.BatchNorm2d(64)
+        self.relu = nn.ReLU(inplace=True)
+        self.CoRe = nn.Sequential(
+            DictConv2d(in_channels=64, out_channels=64, kernel_size=3, stride=1, padding=1,bias=False),
+            nn.BatchNorm2d(64)
+            )
         self.maxpool = nn.MaxPool2d(kernel_size=3, stride=2, padding=1)
         self.layer1 = self._make_layer(block, 64, layers[0])
         self.layer2 = self._make_layer(block, 128, layers[1], stride=2)
@@ -194,7 +178,8 @@ class ResNet(nn.Module):
     def forward(self, x):
         x = self.conv1(x)
         x = self.bn1(x)
-        #x = self.relu(x)
+        x = self.relu(x)
+        x = self.CoRe(x)
         x = self.maxpool(x)
 
         x = self.layer1(x)
@@ -267,3 +252,4 @@ def resnet152(pretrained=False, **kwargs):
     if pretrained:
         model.load_state_dict(model_zoo.load_url(model_urls['resnet152']))
     return model
+
